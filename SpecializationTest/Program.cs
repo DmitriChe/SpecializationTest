1deb97e110705aa6a6a66f41241120e50c208d3e
DmitriChe <juneority@mail.ru>
2023-01-30 03:30:42 +0300

    Написана программа фильтрации массива Program.csg

--- SpecializationTest/Program.cs
+++ SpecializationTest/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// контрольная для специализации
+string[] array = {"Привет", "глашатай", "мех", "123", "7", "Да", "блоха", "уж", "!"};
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        count++;
+    }
+}
+string[] arr = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        arr[j] = array[i];
+        j++;
+    }
+}
+string result = string.Empty;
+for (int i = 0; i < count; i++)
+{
+    result += $"{arr[i]} ";
+}
+Console.WriteLine(result);
